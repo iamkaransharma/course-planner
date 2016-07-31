b98a0bbb83892761383522a20c5475a3f0d29515
kcngo <kcngo@sfu.ca>
2016-07-31 01:51:02 -0700

    Working on CSV Scanner

--- src/ca/cmpt213/courseplanner/model/CSVReader.java
+++ src/ca/cmpt213/courseplanner/model/CSVReader.java
@@ -1,7 +1,30 @@
 package ca.cmpt213.courseplanner.model;
 
+import java.io.*;
+import java.util.*;
+
 /**
  * Created by Thomas_Ngo on 2016-07-30.
  */
 public class CSVReader {
+
+    private String csvFileName;
+
+    public CSVReader(String csvFileName){
+        this.csvFileName = csvFileName;
+    }
+
+    void scanCSVFile(){
+        try {
+            Scanner csvScanner = new Scanner(new File(csvFileName));
+            while(csvScanner.hasNext()){
+
+            }
+            csvScanner.close();
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
 }
